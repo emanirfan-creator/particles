1e7c65fa29c0c281d21b47020541eacad8debb50
Eman Irfan <emani@MacBook-Air.local>
2026-04-17 18:32:08 +0500

    Auto-commit: 2026-04-17 18:32:08

--- src/three-scene.ts
+++ src/three-scene.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { generate3D } from './shapes';
+import type { ShapeOpts } from './shapes';
 import { samplePalette, hexToRgb } from './palettes';
 import { flowAngle } from './noise';
 import type { ParticleState } from './state';
